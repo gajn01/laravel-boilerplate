bf144b406461ab68cbbebb6fc9a2c6ea2d15edad
gajn01 <garciajuanmiguel1105@gmail.com>
2023-04-11 12:15:34 +0800

    sub subcategory on search on progress

--- app/Http/Livewire/Settings/SubSubCategoryLabel.php
+++ app/Http/Livewire/Settings/SubSubCategoryLabel.php
@@ -12,6 +12,7 @@ use App\Models\SubSubCategoryLabel as SubSubCategoryLabelModel;
 
 class SubSubCategoryLabel extends Component
 {
+    protected $listeners = ['alert-sent' => 'onAlertSent'];
     public $category_name;
     public $category_id;
     public $sub_category_id;
@@ -38,47 +39,39 @@ class SubSubCategoryLabel extends Component
         $this->sub_category_id = $sub_category_id;
         $this->sub_sub_category_id = $sub_sub_category_id;
 
-        /*     $category = CategoryModel::where('id', $category_id)->first();
-        $this->category_name = $category->name;
-        $sub_category = SubCategoryModel::where('id', $sub_category_id)->first();
-        $this->sub_category_name = $sub_category->name;
-        $sub_sub_category_label = SubCategoryLabelModel::where('id', $sub_sub_category_id)->first();
-        $this->sub_sub_category_name = $sub_sub_category_label->name;
-        $this->label_list = SubSubCategoryLabelModel::where('sub_sub_category_id', $sub_sub_category_id)->get()->toArray();
-        */
-        $data = CategoryModel::with([
-            'subCategories' => function ($query) use ($sub_category_id) {
-                $query->where('id', $sub_category_id);
-            },
-            'subCategories.labels' => function ($query) use ($sub_sub_category_id) {
-                $query->where('id', $sub_sub_category_id);
-            },
-            'subCategories.labels.subSubCategoryLabels'
-        ])->find($category_id);
+        $data = DB::table('categories')
+            ->join('sub_categories', 'categories.id', '=', 'sub_categories.category_id')
+            ->join('sub_categories_label', 'sub_categories.id', '=', 'sub_categories_label.sub_category_id')
+            ->join('sub_sub_categories', 'sub_categories_label.id', '=', 'sub_sub_categories.sub_sub_category_id')
+            ->select(
+                'categories.name as category_name',
+                'sub_categories.name as sub_category_name',
+                'sub_categories_label.name as sub_sub_category_name',
+                'sub_sub_categories.*',
+            )
+            ->where('categories.id', '=', $category_id)
+            ->where('sub_categories.id', '=', $sub_category_id)
+            ->where('sub_categories_label.id', '=', $sub_sub_category_id)
+            ->get();
 
-        $this->category_name = $data->name;
-        $subCategory = $data->subCategories->first();
-        $this->sub_category_name = $subCategory->name;
-        $subCategoryLabel = $subCategory->labels->first();
-        $this->sub_sub_category_name = $subCategoryLabel->name;
-        $this->label_list = $subCategoryLabel->subSubCategoryLabels->toArray();
+        $this->category_name = $data[0]->category_name;
+        $this->sub_category_name = $data[0]->sub_category_name;
+        $this->sub_sub_category_name = $data[0]->sub_sub_category_name;
+        $this->label_list = json_decode(json_encode($data->toArray()), true);
 
     }
     public function showModal($label_id = null)
     {
-        if ($label_id) {
-            $label = SubSubCategoryLabelModel::findOrFail($label_id);
-            $this->name = $label->name;
-            $this->bp = $label->bp;
-            $this->is_all_nothing = $label->is_all_nothing;
-        }
+        $label = SubSubCategoryLabelModel::find($label_id);
+        $this->name = optional($label)->name;
+        $this->bp = optional($label)->bp;
+        $this->is_all_nothing = optional($label)->is_all_nothing ?? false;
         $this->resetValidation();
         $this->label_id = $label_id;
-        $this->modalTitle = $this->label_id ? 'Edit Label' : 'Add Label';
-        $this->modalButtonText = $this->label_id ? 'Update' : 'Add';
+        $this->modalTitle = $label_id ? 'Edit Label' : 'Add Label';
+        $this->modalButtonText = $label_id ? 'Update' : 'Add';
         $this->dispatchBrowserEvent('show-item-form');
     }
-
 
     public function onSave()
     {
@@ -88,20 +81,26 @@ class SubSubCategoryLabel extends Component
             'is_all_nothing' => $this->is_all_nothing,
             'bp' => $this->bp
         ];
-
         $label = SubSubCategoryLabelModel::updateOrCreate(['id' => $this->label_id], $labelData);
-
-        $subCategoryLabel = $label->subCategoryLabel;
         $this->label_list = SubSubCategoryLabelModel::where('sub_sub_category_id', $this->sub_sub_category_id)
-            ->when($subCategoryLabel, function ($query) {
+            ->when($label->subCategoryLabel, function ($query) {
                 return $query->with('someRelationship');
             })
             ->get()
             ->toArray();
-
         $this->reset();
         $this->onAlert(false, 'Success', 'Label saved successfully!', 'success');
         $this->dispatchBrowserEvent('remove-modal', ['modalName' => '#sub_category_label_modal']);
+        $this->emit('saved');
+    }
+
+    public function onDelete($id)
+    {
+        $store = SubSubCategoryLabelModel::find($id);
+        $store->delete();
+        $this->label_list = array_values(array_filter($this->label_list, function ($label) use ($id) {
+            return $label['id'] != $id;
+        }));
         $this->emit('saved');
     }
 
