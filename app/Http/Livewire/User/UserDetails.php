dc2fe69f16b40ff4a4fe2e8a4fe779838949c75b
gajn01 <garciajuanmiguel1105@gmail.com>
2023-04-15 16:53:03 +0800

    user data fetching done

--- app/Http/Livewire/User/UserDetails.php
+++ app/Http/Livewire/User/UserDetails.php
@@ -4,13 +4,22 @@ namespace App\Http\Livewire\User;
 
 use Livewire\Component;
 use App\Models\User as UserModel;
+use Livewire\WithPagination;
+use App\Helpers\CustomHelper;
 
 class UserDetails extends Component
 {
+    public $account_id;
     public $employee_id;
     public $name;
+    public $input_name;
+    public $input_email;
+    public $input_status;
     public $email;
     public $status;
+    public $is_edit = false;
+    public $modalButtonText;
+
     public function render()
     {
         return view('livewire.user.user-details')->extends('layouts.app');
@@ -18,8 +27,48 @@ class UserDetails extends Component
     public function mount($employee_id = null){
         $this->employee_id = $employee_id;
         $data = UserModel::where('employee_id', $this->employee_id)->first();
-        $this->name = $data->name;
+        $this->account_id = optional($data)->id;
+        $this->name = optional($data)->name;
+        $this->email = optional($data)->email;
+        $this->status = optional($data)->status;
+    }
+    public function onUpdate($boolean){
+        $this->input_name = $this->name;
+        $this->input_email = $this->email;
+        $this->input_status = $this->status;
+        $this->is_edit = $boolean;
+    }
 
-
+    public function onSaveAccount()
+    {
+        $this->validate(
+            [
+                'name' => 'required|max:255',
+                'email' => 'required|email',
+            ]
+        );
+        UserModel::updateOrCreate(
+            ['id' => $this->account_id ?? null],
+            [
+                'name' => strip_tags($this->input_name),
+                'email' => strip_tags($this->input_email),
+                'status' => strip_tags($this->input_status),
+            ]
+        );
+        $this->resetValidation();
+        $this->onAlert(false, 'Success', 'Account saved successfully!', 'success');
+        $this->onUpdate(false);
+    }
+    public function onTogglePassword(){
+      /*   $this->is_toggle = !$this->is_toggle; */
+        $this->emit('toggleEye');
+    }
+    public function onAlert($is_confirm = false, $title = null, $message = null, $type = null, $data = null)
+    {
+        CustomHelper::onShow($this, $is_confirm, $title, $message, $type, $data);
+    }
+    public function reset(...$properties)
+    {
+        $this->resetValidation();
     }
 }
--- resources/views/livewire/user/user-details.blade.php
+++ resources/views/livewire/user/user-details.blade.php
@@ -4,85 +4,110 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ route('user') }}">User</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{ $name }}</li>
+            <li class="breadcrumb-item active" aria-current="page" wire:ignore>{{ $name }}</li>
         </ol>
     </nav>
 
     <div class="row gy-4 mb-3">
-        <!--//col-->
         <div class="col-12 col-lg-6">
-            <div class="app-card app-card-account shadow-sm d-flex flex-column align-items-start">
-                <div class="app-card-header p-3 border-bottom-0">
-                    <div class="row align-items-center gx-3">
-                        <div class="col-auto">
-                        </div>
-                        <!--//col-->
+            <div class="app-card app-card-chart h-100 shadow-sm">
+                <div class="app-card-header p-3">
+                    <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
                             <h4 class="app-card-title">Profile</h4>
+                        </div>
+                        <!--//col-->
+                        <div class="col-auto ">
+                            <div class="card-header-action col ">
+                                <a class="btn app-btn-primary" wire:click="onUpdate(true)">Edit</a>
+                            </div>
+                            <!--//card-header-actions-->
                         </div>
                         <!--//col-->
                     </div>
                     <!--//row-->
                 </div>
                 <!--//app-card-header-->
-                <div class="app-card-body px-4 w-100">
+                <div class="app-card-body p-3 ">
+                    <form wire:submit.prevent="onSave">
+                        @csrf
 
-                    <div class="item border-bottom py-3">
-                        <div class="row justify-content-between align-items-center">
-                            <div class="col-auto">
-                                <div class="item-label mb-1"><strong>User ID</strong></div>
-                                <div class="item-data">5358</div>
+                        <div class="item border-bottom py-3">
+                            <div class="row justify-content-between align-items-center">
+                                <div class="col-auto">
+                                    <div class="item-label mb-1"><strong>Employee ID</strong></div>
+                                    <div class="item-data">5358</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="item border-bottom py-3">
-                        <div class="row justify-content-between align-items-center">
-                            <div class="col-auto">
-                                <div class="item-label mb-1"><strong>Full Name</strong></div>
-                                <div class="item-data">Juan Miguel Garcia</div>
+                        <div class="item border-bottom py-3">
+                            <div class="row ">
+                                <div class="col-12">
+                                    <div class="item-label mb-1"><strong>Full Name</strong></div>
+                                    @if ($is_edit)
+                                        <input type="text" class="form-control" wire:model="input_name" id="input_name" value="{{ $name}}">
+                                        @error('input_name')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    @else
+                                        <div class="item-data">{{ $name }}</div>
+                                    @endif
+
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="item border-bottom py-3">
-                        <div class="row justify-content-between align-items-center">
-                            <div class="col-auto">
-                                <div class="item-label mb-1"><strong>Email</strong></div>
-                                <div class="item-data">garciajuanmiguel1105@gmail.com</div>
-                                {{--  <form class="docs-search-form row gx-1 align-items-center">
-                                    <div class="col-auto">
-                                        <input type="text" id="search-docs" name="searchdocs" class="form-control search-docs"/>
-                                    </div>
-                                </form> --}}
+                        <div class="item border-bottom py-3">
+                            <div class="row ">
+                                <div class="col-12">
+                                    <div class="item-label mb-1"><strong>Email</strong></div>
+                                    @if ($is_edit)
+                                        <input type="text" class="form-control" wire:model="input_email" id="input_email">
+                                        @error('input_email')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                    @else
+                                        <div class="item-data">{{ $email }}</div>
+                                    @endif
+                                </div>
                             </div>
-
-                            <!--//col-->
-                            <div class="col text-end">
-                                <a class="btn-sm app-btn-secondary" href="#">Update</a>
-                            </div>
-                            <!--//col-->
                         </div>
-                        <!--//row-->
-                    </div>
 
-                    <div class="item border-bottom py-3">
-                        <div class="row justify-content-between align-items-center">
-                            <div class="col-auto">
+                        <div class="item py-3">
+                            <div class="row justify-content-between align-items-center">
                                 <div class="item-label mb-1"><strong>Status</strong></div>
-                                <div class="item-data"><span class="badge bg-success">Active</span></div>
-                            </div>
+                                <div class="col-12">
+                                    @if ($is_edit)
 
-                            <!--//col-->
-                            <div class="col text-end">
-                                <a class="btn-sm app-btn-secondary" href="#">Update</a>
+                                        <select class="form-select form-select-md" name="input_status" id="input_status"
+                                            wire:model="input_status">
+                                            <option selected>Select status</option>
+                                            <option value="0">Pending</option>
+                                            <option value="1">Active</option>
+                                            <option value="2">Inactive</option>
+                                        </select>
+                                    @else
+                                        @if ($status == 1)
+                                            <span class="badge bg-success">Active</span>
+                                        @elseif($status == 0)
+                                            <span class="badge bg-warning">Pending</span>
+                                        @else
+                                            <span class="badge bg-danger">Inactive</span>
+                                        @endif
+                                    @endif
+                                </div>
                             </div>
-                            <!--//col-->
                         </div>
-                    </div>
-
+                        <div class="app-card-footer pt-2 mt-auto text-end {{ $is_edit ? '' : 'd-none' }}">
+                            <button type="button" class="btn btn-secondary" wire:click="onUpdate(false)">Cancel</button>
+                            <button type="button" class="btn app-btn-primary" wire:click="onSaveAccount">Save</button>
+                        </div>
+                    </form>
                 </div>
+                <!--//app-card-body-->
             </div>
+            <!--//app-card-->
             <!--//app-card-->
         </div>
         <div class="col-12 col-lg-6">
@@ -91,18 +116,24 @@
                     <div class="row justify-content-between align-items-center">
                         <div class="col-auto">
                             <h4 class="app-card-title">Schedule</h4>
-                        </div><!--//col-->
+                        </div>
+                        <!--//col-->
                         <div class="col-auto">
                             <div class="card-header-action col ">
-                                <a class="btn-sm app-btn-secondary" href="#" data-bs-toggle="modal" data-bs-target="#assignModal" >Assign Store</a>
-                            </div><!--//card-header-actions-->
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//app-card-header-->
-                <div class="app-card-body p-3 p-lg-4">
+                                <a class="btn-sm app-btn-secondary" href="#" data-bs-toggle="modal"
+                                    data-bs-target="#assignModal">Assign Store</a>
+                            </div>
+                            <!--//card-header-actions-->
+                        </div>
+                        <!--//col-->
+                    </div>
+                    <!--//row-->
+                </div>
+                <!--//app-card-header-->
+                <div class="app-card-body p-3 ">
                     <div class="mb-3 d-flex">
                         <select class="form-select form-select-sm ms-auto d-inline-flex w-auto">
-                            <option value="1" >This week</option>
+                            <option value="1">This week</option>
                             <option value="2 " selected>Today</option>
                             <option value="3">This Month</option>
                             <option value="3">This Year</option>
@@ -121,16 +152,23 @@
                                 <td class="cell">April 23 2023</td>
                                 <td class="cell">SM Tanza</td>
                                 <td class="cell">
-                                    <a href="#" data-bs-toggle="modal" data-bs-target="#assignModal" data-toggle="tooltip" data-placement="top" title="Update">
-                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
+                                    <a href="#" data-bs-toggle="modal" data-bs-target="#assignModal"
+                                        data-toggle="tooltip" data-placement="top" title="Update">
+                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                            <path
+                                                d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+                                        </svg>
                                     </a>
                                 </td>
                             </tbody>
                         </table>
                     </div>
 
-                </div><!--//app-card-body-->
-            </div><!--//app-card-->
+                </div>
+                <!--//app-card-body-->
+            </div>
+            <!--//app-card-->
             <!--//app-card-->
         </div>
     </div>
@@ -165,7 +203,11 @@
                         <td class="cell">Wave 1</td>
                         <td class="cell">
                             <a href="" data-toggle="tooltip" data-placement="top" title="View">
-                                <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
+                                <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                    <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path
+                                        d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
+                                </svg>
                             </a>
                         </td>
                     </tbody>
@@ -177,12 +219,13 @@
     </div>
 
     <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-    <div class="modal fade" id="assignModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+    <div class="modal fade" id="assignModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"
+        role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
         <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalTitleId">Assign Store</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
 
@@ -197,9 +240,9 @@
                     </div>
 
                     <div class="mb-3">
-                      <label for="" class="form-label">Date of Audit</label>
-                      <input type="date"
-                        class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                        <label for="" class="form-label">Date of Audit</label>
+                        <input type="date" class="form-control" name="" id=""
+                            aria-describedby="helpId" placeholder="">
                     </div>
                 </div>
                 <div class="modal-footer">
