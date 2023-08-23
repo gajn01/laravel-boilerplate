6a6341b1fbcb4f7e9bd456b8685d69c0ff457efd
gajn01 <garciajuanmiguel1105@gmail.com>
2023-08-23 14:18:04 +0800

    insight done

--- app/Http/Livewire/Report/Summary.php
+++ app/Http/Livewire/Report/Summary.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\Report;
 use Livewire\Component;
 use App\Models\Store as StoreModel;
 use App\Models\Category as CategoryModel;
+use App\Models\SubCategory;
+use App\Models\SubCategoryLabel;
 use App\Models\AuditFormResult as AuditFormResultModel;
 use Livewire\WithPagination;
 use Illuminate\Support\Facades\Response;
@@ -13,35 +15,49 @@ class Summary extends Component
 {
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
-    public $category, $area, $store, $search, $wave, $type;
+    public $category, $subCategory,$subSubCategory,$area, $store, $search, $wave, $type;
     public $store_list;
     public $limit = 10;
     public function render()
     {
         $category = CategoryModel::all()->unique('name');
         $summary = $this->getSummaryList();
-        return view('livewire.report.summary', ['summary' => $summary, 'categories' => $category])->extends('layouts.app');
+        $subCategoryList = $this->getSubCategory($this->category);
+        $subSubCategoryList = $this->getSubSubCategory($this->subCategory);
+        return view('livewire.report.summary', ['total'=>$summary['totalResults'],'summary' => $summary['results'], 'categories' => $category,'subCategoryList'=> $subCategoryList,'subSubCategoryList'=> $subSubCategoryList ])->extends('layouts.app');
     }
-    public function getSummaryList(){
-        return AuditFormResultModel::where(fn($query) =>
-        $query->whereHas('forms', fn($q) =>
-            $q->where('audit_status', '2')))
-        ->where(fn($q) =>
+    public function getSummaryList()
+    {
+        $query = AuditFormResultModel::whereHas('forms', fn ($q) =>
+            $q->where('audit_status', '2')
+                ->when($this->area && $this->area !== 'all', fn ($q) => $q->where('area', $this->area))
+                ->when($this->wave && $this->wave !== 'all', fn ($q) => $q->where('wave', $this->wave))
+        )
+        ->where(fn ($q) =>
             $q->whereNotNull('sub_sub_remarks')
                 ->orWhereNotNull('sub_sub_deviation')
                 ->orWhereNotNull('label_remarks')
-                ->orWhereNotNull('label_deviation'))
-        ->when($this->search, fn($query) => $query->whereHas('forms.stores', fn($q) => $q->where('name', 'like', '%' . $this->search . '%')))
-        ->when($this->area && $this->area !== 'all', fn($query) => $query->whereHas('forms.stores', fn($q) => $q->where('area', $this->area)))
-        ->when($this->wave && $this->wave !== 'all', fn($query) => $query->whereHas('forms', fn($q) => $q->where('wave', $this->wave)))
-        ->when($this->category && $this->category !== 'all', fn($query) => $query->where('category_name', $this->category))
-        ->paginate($this->limit);
+                ->orWhereNotNull('label_deviation')
+        )
+        ->when($this->search, fn ($q) => $q->whereHas('forms.stores', fn ($q) => $q->where('name', 'like', '%' . $this->search . '%')))
+        ->when($this->category && $this->category !== 'all', fn ($q) => $q->where('category_id', $this->category))
+        ->when($this->subCategory && $this->subCategory !== 'all', fn ($q) => $q->where('sub_category_id', $this->subCategory))
+        ->when($this->subSubCategory && $this->subSubCategory !== 'all', fn ($q) => $q->where('sub_sub_category_id', $this->subSubCategory));
+        $totalResults = $query->count();
+        $results = $query->paginate($this->limit);
+        return ['totalResults' => $totalResults, 'results' => $results];
+    }
+    public function getSubCategory($category_id){
+        return SubCategory::where('category_id', $category_id)->get();
+    }
+    public function getSubSubCategory($sub_category_id){
+        return SubCategoryLabel::where('sub_category_id', $sub_category_id)->get();
     }
     public function exportCSV(){
         $summary_list = $this->getSummaryList();
         $csvData = [];
         $csvData[] = ['Area', 'Store Name', 'ROS', 'Category', 'Sub-Category', 'Specific', 'Deviation Details', 'Remarks','Additional Info','Year','Wave']; 
-        foreach ($summary_list as $summary) {
+        foreach ($summary_list['results'] as $summary) {
             $csvData[] = [
                 $summary->forms->stores->area,
                 $summary->forms->stores->name,
--- resources/views/livewire/report/summary.blade.php
+++ resources/views/livewire/report/summary.blade.php
@@ -9,17 +9,15 @@
     <h1 class="app-page-title mb-0">Summary</h1>
     <div class="page-utilities mb-3">
         <div class="row g-2 justify-content-start justify-content-md-end align-items-center">
-            <div class="col-auto">
+          {{--   <div class="col-auto">
                 <svg data-bs-toggle="collapse" data-bs-target="#collapseFilter" aria-expanded="true"
                     aria-controls="collapseFilter" data-bs-toggle="tooltip" data-bs-placement="top" title="Filter"
                     class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path
-                        d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
+                    <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
                 </svg>
-            </div>
+            </div> --}}
             <div class="col-auto">
-                <input type="text" id="search-docs" name="searchdocs" class="form-control search-docs"
-                    wire:model.debounce.100ms="search" placeholder="Search Store">
+                <input type="text" id="search-docs" name="searchdocs" class="form-control search-docs" wire:model.debounce.100ms="search" placeholder="Search Store">
             </div>
             <div class="col-auto">
                 <div class="col-auto">
@@ -28,7 +26,74 @@
             </div>
         </div>
     </div>
-    <div class="accordion" id="accordionFilter" wire:ignore>
+    <div class="page-utilities secondary-bg p-3 mb-2">
+        <div class="row g-2 justify-content-start justify-content-md-start align-items-center">
+            <div class="col-auto d-flex flex-column">
+                <label for="type" class="form-label">Store Type</label>
+                <select name="type" id="type"
+                    class="form-select form-select-md ms-auto d-inline-flex w-auto" wire:model="type">
+                    <option value hidden selected>--Select Type--</option>
+                    <option value="all">All</option>
+                    <option value="1">Cafe</option>
+                    <option value="2">Kiosk</option>
+                </select>
+            </div>
+            <div class="col-auto d-flex flex-column">
+                <label for="area" class="form-label">Area</label>
+                <select name="area" id="area"
+                    class="form-select form-select-md ms-auto d-inline-flex w-auto" wire:model="area">
+                    <option value hidden selected>--Select Area--</option>
+                    <option value="all">All</option>
+                    <option value="South">South</option>
+                    <option value="North">North</option>
+                    <option value="MFO">MFO</option>
+                </select>
+            </div>
+            <div class="col-auto d-flex flex-column">
+                <label for="store" class="form-label">Category</label>
+                <select class="form-select form-select-sm ms-auto d-inline-flex w-auto" wire:model="category">
+                    <option value hidden selected>--Select Category--</option>
+                    <option value="">All</option>
+                    @foreach ($categories as $item)
+                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-auto d-flex flex-column">
+                <label for="store" class="form-label">Sub Category</label>
+                <select class="form-select form-select-sm ms-auto d-inline-flex w-auto" wire:model="subCategory">
+                    <option value hidden selected>--Select Sub Category--</option>
+                    <option value="">All</option>
+                    @forelse ($subCategoryList as $item)
+                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                    @empty
+                    @endforelse
+                </select>
+            </div>
+            <div class="col-auto d-flex flex-column">
+                <label for="store" class="form-label">Sub Sub Category</label>
+                <select class="form-select form-select-sm ms-auto d-inline-flex w-auto" wire:model="subSubCategory">
+                    <option value hidden selected>--Select Sub Sub Category--</option>
+                    <option value="">All</option>
+                    @forelse ($subSubCategoryList as $item)
+                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                    @empty
+                    @endforelse
+                </select>
+            </div>
+            <div class="col-auto d-flex flex-column">
+                <label for="wave" class="form-label">Wave</label>
+                <select name="wave" id="wave"
+                    class="form-select form-select-md ms-auto d-inline-flex w-auto" wire:model="wave">
+                    <option value hidden selected>--Select Wave--</option>
+                    <option value="all">All</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+            </div>
+        </div>
+    </div>
+   {{--  <div class="accordion" id="accordionFilter" >
         <div class="accordion-item secondary-bg border-0">
             <div id="collapseFilter" class="accordion-collapse collapse " aria-labelledby="headingOne"
                 data-bs-parent="#accordionFilter">
@@ -62,8 +127,21 @@
                                     <option value hidden selected>--Select Category--</option>
                                     <option value="">All</option>
                                     @foreach ($categories as $item)
-                                        <option value="{{ $item->name }}">{{ $item->name }}</option>
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
                                     @endforeach
+                                </select>
+                            </div>
+                            <div class="col-auto d-flex flex-column">
+                                <label for="store" class="form-label">Sub Category</label>
+                                <select class="form-select form-select-sm ms-auto d-inline-flex w-auto" wire:model="subCategory">
+                                    <option value hidden selected>--Select Sub Category--</option>
+                                    <option value="">All</option>
+                                    @forelse ($subCategoryList as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @empty
+
+                                    @endforelse
+                                   
                                 </select>
                             </div>
                             <div class="col-auto d-flex flex-column">
@@ -76,17 +154,13 @@
                                     <option value="2">2</option>
                                 </select>
                             </div>
-                       {{--      <div class="col-auto d-flex flex-column">
-                                <label for="pickup_date" class="form-label">Pick-up Date</label>
-                                <input type="date" class="form-control" name="pickup_date" id="pickup_date" wire:model="date_pickup_filter">
-                            </div> --}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="app-card app-card-orders-table shadow-sm mb-5">
+    </div> --}}
+    <div class="app-card app-card-orders-table shadow-sm mb-3">
         <div class="app-card-body">
             <div class="table-responsive">
                 <table class="table app-table-hover mb-0 text-left">
@@ -115,9 +189,7 @@
                                 <td class="cell">{{ $results->sub_name }}</td>
                                 <td class="cell">{{ $results->sub_sub_name }}</td>
                                 <td class="cell">{{ $results->label_name }}</td>
-                                <td class="cell">
-                                    {{ $results->sub_sub_remarks ? $results->sub_sub_remarks : $results->label_remarks }}
-                                </td>
+                                <td class="cell">{{ $results->sub_sub_remarks ? $results->sub_sub_remarks : $results->label_remarks }}</td>
                                 <td class="cell">{{ $results->sub_sub_deviation ? $results->sub_sub_deviation : $results->label_deviation  }}</td>
                                 <td class="cell">{{ date('Y', strtotime($results->updated_at)) }}</td>
                                 <td class="cell text-center">{{ $results->forms->wave }}</td>
@@ -139,6 +211,9 @@
             </div>
         </div>
     </div>
+    <div class="row mb-3">
+        <h5 class="fw-bold text-end ">Total: {{$total}}</h5>
+    </div>
     {{-- Pagination --}}
     <div class="row">
         <div class="col-sm-12 col-md-6">
@@ -153,10 +228,11 @@
                             <option value="20">20</option>
                             <option value="50">50</option>
                             <option value="100">100</option>
+                            <option value="0">All</option>
                         </select>
                     </div>
                     <div class="col-auto">
-                        <label for="">entries</label>
+                        <label >entries</label>
                     </div>
                 </div>
             </div>
